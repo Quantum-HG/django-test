81d2350bda1e6129bf757360b18d24a83f37c3d7
Quantum-HG <hgquantum24@gmail.com>
2025-06-07 14:42:03 +0530

    build

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+if [[$CREATE_SUPERUSER]];
+then
+  python manage.py createsuperuser --no-input --email "$DJANGO_SUPER_EMAIL"
