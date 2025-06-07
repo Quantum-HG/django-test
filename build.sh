e5a4d03f726310177d08fc21b86cfe7be2b6f6ff
Quantum-HG <hgquantum24@gmail.com>
2025-06-07 15:23:50 +0530

    build

--- build.sh
+++ build.sh
@@ -3,9 +3,12 @@ set -o errexit
 
 pip install -r requirements.txt
 
+mkdir -p staticfiles
+
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
 if [[$CREATE_SUPERUSER]];
 then
   python manage.py createsuperuser --no-input --email "$DJANGO_SUPER_EMAIL"
--- django_project/settings.py
+++ django_project/settings.py
@@ -128,9 +128,6 @@ if not DEBUG:
     # and renames the files with unique names for each version to support long-term caching
     # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
